abf1de629812a50bf00c0e427fcd0fc2292af1e5
wzhy1997 <yu891223>
2022-07-22 21:24:23 +0800

    2-7 增加热部署

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,17 +1,20 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello}")
+    private String testHello;
 @RequestMapping("/hello")
     public String hello(){
     return "ok";
 }
-    @PostMapping("/h")
+    @PostMapping("/h1")
     public String h(String name){
-        return name;
+        return name+testHello;
     }
 }
